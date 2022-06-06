269cb1f404084d6606243ca683e68649201a9ae7
Mikko Ohtamaa <mikko@opensourcehacker.com>
2022-06-06 18:35:31 +0200

    Bump

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 #
-# Release the package with npm
+# Manually create the package with npm
 # https://github.com/tradingstrategy-ai/trade-executor-frontend/pull/1
+# https://github.com/tradingstrategy-ai/frontend/pull/117
 #
 npm version patch
 git push
