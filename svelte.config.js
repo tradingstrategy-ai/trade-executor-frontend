daa31af47e047c0b0f9479b5598a48dd8581c028
Ken Kunz <kennethkunz@gmail.com>
2022-11-17 12:55:31 -0600

    upgrade SvelteKit to next.405
    - update %sveltekit.body% and %sveltekit.body% in app.html
    - import vite 3.x; extract vite configs to vite.config.js
    - use new env variable API
    - update various configs to match sveltekit init template

--- svelte.config.js
+++ svelte.config.js
@@ -1,33 +1,28 @@
-import path from 'path';
 import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: preprocess(),
+	// Disable all warnings during the local compilation for now (spammy)
+	onwarn: () => {},
 
 	kit: {
 		adapter: adapter(),
 
-		// Override http methods in the Todo forms
-		methodOverride: {
-			allowed: ['PATCH', 'DELETE']
+		alias: {
+			'trade-executor-frontend': 'src/lib'
+		},
+
+		env: {
+			publicPrefix: 'TS_PUBLIC_'
 		},
 
 		package: {
 			exports: (filepath) => !/^_|\/_|\.d\.ts|\.svelte$/.test(filepath)
-		},
-
-		vite: {
-			resolve: {
-				alias: {
-					'trade-executor-frontend': path.resolve('src/lib')
-				}
-			}
 		}
-	}
+	},
+
+	preprocess: preprocess()
 };
 
 export default config;
