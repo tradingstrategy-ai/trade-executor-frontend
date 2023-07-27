5e75bbc21e51a4a926904dfe083135e091576e84
Mikko Ohtamaa <mikko@opensourcehacker.com>
2023-07-27 10:27:50 +0300

    Adding more data structure descriptions

--- src/lib/state/interface.ts
+++ src/lib/state/interface.ts
@@ -17,6 +17,16 @@ export type BlockNumber = number;
 
 export type HexString = `0x${string}`;
 
+export type USDollarPrice = number;
+
+export type USDollarValue = number;
+
+// How many tokens in the token units. E.g. 0.02 WMATIC
+export type TokenUnits = number;
+
+// Percent value 0...1
+export type Percent = number;
+
 /**
  * Used to differetiate different position types in UI logic
  */
@@ -24,6 +34,17 @@ export enum PositionKind {
 	open,
 	closed,
 	frozen
+}
+
+export interface AssetIdentifier {
+	token_symbol: string;
+	internal_id: PrimaryKey;
+}
+
+export interface TradingPairIdentifier {
+	base: AssetIdentifier;
+	quote: AssetIdentifier;
+	internal_id: PrimaryKey;
 }
 
 /**
@@ -40,15 +61,40 @@ export interface BlockchainTransaction {
 }
 
 /**
+ * Estimated pricing of a trade
+ */
+export interface PriceStructure {
+	// Estimated price we are going to get when we trade with our size.
+	// This includes price impact, but not slippage.
+	price: USDollarPrice;
+
+	// Market mid-price at the time of the trade decision
+	mid_price: USDollarPrice;
+
+	// List of different liquidity provider fees we need to pay per each leg of a multihop trade we do
+	lp_fee: USDollarPrice[];
+}
+
+/**
  *
  * https://github.com/tradingstrategy-ai/trade-executor/blob/master/tradeexecutor/state/trade.py
  */
 export interface TradeExecution {
 	trade_id: PrimaryKey;
 	position_id: PrimaryKey;
+
+	pair: TradingPairIdentifier;
+
+	executed_at: UnixTimestamp;
 	failed_at: UnixTimestamp;
-	pair: any;
+
 	blockchain_transactions: BlockchainTransaction[];
+
+	assumed_price: USDollarPrice;
+	executed_price: USDollarPrice;
+	executed_quantity: TokenUnits;
+
+	price_structure: PriceStructure;
 }
 
 /**
@@ -56,11 +102,16 @@ export interface TradeExecution {
  * https://github.com/tradingstrategy-ai/trade-executor/blob/e1d3a1df80d8aa6ce505507253af8c4ed87cb222/tradeexecutor/state/position.py#L18
  */
 export interface TradingPosition {
-	pair: any;
+	pair: TradingPairIdentifier;
 	trades: Record<PrimaryKey, TradeExecution>;
 	position_id: PrimaryKey;
+	opened_at: UnixTimestamp;
+	closed_at: UnixTimestamp;
 	frozen_at: UnixTimestamp;
 	unfrozen_at: UnixTimestamp;
+	portfolio_value_at_open: USDollarValue;
+	last_token_price: USDollarPrice;
+	stop_loss: USDollarPrice;
 }
 
 /**
